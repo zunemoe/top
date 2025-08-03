bcc9540446b5d02699264f781784f4aec41b2920
zunemoe <2897539+zunemoe@users.noreply.github.com>
2025-08-03 20:10:37 +1200

    initialize binary search trees

---
+++ projects/binary-search-trees/binary-search-trees.js
@@ -0,0 +1,62 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+        this.size = 0;
+    }
+
+    buildTree(array) {
+
+    }
+
+    insert(value) {
+
+    }
+
+    delete(value) {
+
+    }
+
+    find(value) {
+
+    }
+
+    levelOrderForEach(callback) {
+
+    }
+
+    inOrderForEach(callback) {
+
+    }
+
+    preOrderForEach(callback) {
+
+    }
+
+    postOrderForEach(callback) {
+
+    }
+
+    height(value) {
+
+    }
+
+    depth(value) {
+
+    }
+
+    isBalanced() {
+
+    }
+
+    rebalance() {
+        
+    }
+}
