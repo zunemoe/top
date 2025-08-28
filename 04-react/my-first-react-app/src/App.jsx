c2a1ab8a621bedc14039a8550f610559a4891f34
zunemoe <2897539+zunemoe@users.noreply.github.com>
2025-08-28 23:04:21 +1200

    Enhance App component with dynamic background color and click counter

--- 04-react/my-first-react-app/src/App.jsx
+++ 04-react/my-first-react-app/src/App.jsx
@@ -3,17 +3,38 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const COLORS = ['red', 'green', 'blue', 'yellow', 'purple'];
+
 function App() {
-  const names = ['Alice', 'Bob', 'Charlie'];
+  const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
+  const [clickCount, setClickCount] = useState(0);
+  const onButtonClick = (color) => () => {
+    setClickCount(prev => prev + 1);
+    setBackgroundColor(color);
+  };
+
+  const names = ['Alice', 'Bob', 'Charlie'];  
   // const nameElements = names.map(name => <li key={name}>{name}</li>);
 
   return (
-    <div>
+    <div className="App" style={{ backgroundColor }}>
+      {COLORS.map((color) => (
+        <button
+          type="button"
+          key={color}
+          onClick={onButtonClick(color)}
+          className={backgroundColor === color ? "selected" : ""}
+        >
+          {color}
+        </button>
+      ))}
+      <p>Button clicked: {clickCount} times.</p>
       <h1>Names</h1>
-      <List names={names}/>
+      <List names={names} />
     </div>
   );
 }
+
 
 function List(props) {
   return (
@@ -29,4 +50,4 @@ function ListItem(props) {
   return <li>{props.name}</li>
 }
 
-export default App
+export default App;
